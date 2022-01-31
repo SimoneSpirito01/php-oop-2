285b5b5ee2487b746f53c602b548c71ff88007aa
SimoneSpirito <simospiri01@gmail.com>
2022-01-31 20:58:24 +0100

    class User

---
+++ classes/User.php
@@ -0,0 +1,67 @@
+<?php
+
+class User {
+    protected $nome;
+    protected $cognome;
+    protected $email;
+    protected $address;
+    protected $card;
+
+    public function __construct($_nome, $_cognome, $_email, $_address)
+    {
+
+        $this->setNome($_nome);
+        $this->setCognome($_cognome);
+        $this->setEmail($_email);
+        $this->setAddress($_address);
+        
+    }
+
+    public function setNome($_nome) {
+        
+        if(strlen($_nome) > 3) {
+            $this->nome = $_nome;
+        }
+    }
+
+    public function getNome() {
+
+        return $this->nome;
+    }
+
+    public function setCognome($_cognome) {
+        
+        if(strlen($_cognome) > 3) {
+            $this->cognome = $_cognome;
+        }
+    }
+
+    public function getCognome() {
+
+        return $this->cognome;
+    }
+
+    public function setEmail($_email) {
+        
+        if(str_contains($_email, '@')) {
+            if(str_contains($_email, '.')) {
+                $this->email = $_email;
+            }
+        }
+    }
+
+    public function getEmail() {
+
+        return $this->email;
+    }
+
+    public function setAddress($_address) {
+        
+        $this->address = $_address;
+    }
+
+    public function getAddress() {
+
+        return $this->address;
+    }
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/classes/User.php';
+require_once __DIR__ . '/classes/Product.php';
+
+
+?>
